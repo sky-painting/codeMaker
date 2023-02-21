8d657ebea8c23ecbb7fe9e5f498c512eb5edbbb8
神帅 <2217011466@qq.com>
2023-02-21 06:09:40 +0000

    删除部分测试代码
    Signed-off-by: 神帅 <2217011466@qq.com>

--- codemaker-parent/codemaker-core/src/test/java/com/tianhua/codemaker/service/expertest/RbcClassLoader.java
+++ codemaker-parent/codemaker-core/src/test/java/com/tianhua/codemaker/service/expertest/RbcClassLoader.java
@@ -21,29 +21,7 @@ public class RbcClassLoader extends URLClassLoader {
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        String path = "jar:file:///Users/dasouche/.m2/repository/com/souche/trade/api/7.0.0-SNAPSHOT/api-7.0.0-SNAPSHOT.jar!/";
-        RbcClassLoader myClassLoader = new RbcClassLoader(path, Thread.currentThread().getContextClassLoader().getParent());
-        Thread.currentThread().setContextClassLoader(myClassLoader);
-
-        Class clazz = Thread.currentThread()
-                .getContextClassLoader()
-                .loadClass("com.souche.trade.api.fullorder.TradeFullOrderFacade");
-        System.out.println(clazz.getName());
-        for (Method method : clazz.getMethods()){
-            System.out.println(method.getName());
-            method.getParameterTypes();
-            for (Class<?> paramType : method.getParameterTypes()){
-                System.out.println( paramType.getName());
-            }
-        }
-
-        Class dtoClass = Thread.currentThread()
-                .getContextClassLoader()
-                .loadClass("com.souche.trade.api.fullorder.req.CreateMainOrderRequestDTO");
-        System.out.println(dtoClass.getName());
-        for (Field field : dtoClass.getDeclaredFields()){
-            System.out.println(field.getName());
-        }
+       //delete test code  
     }
 
 }
